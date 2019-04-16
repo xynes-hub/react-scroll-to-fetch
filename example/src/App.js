c0160416fcd93a2ff7f2b3b64d06d6df3668ef93
Your Name <you@example.com>
2019-04-16 22:23:28 +0530

    scroller works prerfectely with  custom scrollerParent

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,42 @@
 import React, { Component } from 'react'
 
-import ExampleComponent from 'react-scroll-to-fetch'
+import ScrollToFetch from 'react-scroll-to-fetch'
 
 export default class App extends Component {
+  state={
+    tmp:[],
+    finished:false
+  }
+
+  fakeFetch=(n)=>{
+    let tmp=[];
+    for(var i=n;i<n+50;i++){
+      tmp[i]=i;
+    }
+    this.setState({tmp:[...this.state.tmp,...tmp]});
+    if(i >150){
+      this.setState({finished:true})
+    }
+  }
+  fetch=page=>{
+    console.log(page);
+    return new Promise((resolve)=>{
+      setTimeout(()=>{this.fakeFetch((page-1)*50);resolve()},1000)
+    })
+  }
   render () {
     return (
-      <div>
-        <ExampleComponent text='Modern React component module' />
+      <div style={{height:300,backgroundColor:'red',overflowY:'scroll'}} id="scroll">
+        
+        <ScrollToFetch
+        fetch={this.fetch} 
+        finished={this.state.finished}
+        loader={<div style={{textAlign:'center'}}>Loading...</div>}
+        successMessage={<div style={{textAlign:'center'}}>No more data to load</div>}
+        scrollParent="scroll"
+        >{this.state.tmp.map((v,i)=>{
+          return <div key={i}>this element no {v}</div>
+        })}</ScrollToFetch>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,21 +1,77 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-
-import styles from './styles.css'
-
-export default class ExampleComponent extends Component {
+require("babel-core/register");
+require("babel-polyfill");
+export default class ScrollToFetch extends Component {
   static propTypes = {
-    text: PropTypes.string
+    finished: PropTypes.bool.isRequired,
+    loader:PropTypes.element,
+    successMessage:PropTypes.element,
+    fetch:PropTypes.func.isRequired,
+    scrollParent:PropTypes.string
+  }
+  state={
+    fetching:false,
+    page:1
+  }
+  async componentDidMount(){
+    if(!this.props.scrollParent){
+      document.addEventListener('scroll',this.trackBottom);
+    }else{
+      document.getElementById(this.props.scrollParent).addEventListener('scroll',this.trackBottom);
+    }
+    
+    this.setState({fetching:true});
+    await this.props.fetch(this.state.page);
+    this.setState({fetching:false,page:this.state.page+1})
+  }
+  componentWillUnmount(){
+    if(!this.props.scrollParent){
+      document.removeEventListener('scroll',this.trackBottom);
+    }else{
+      document.getElementById(this.props.scrollParent).removeEventListener('scroll',this.trackBottom);
+    }
+  } 
+  trackBottom= async ()=>{
+    console.log("scroll event")
+    if(!this.props.finished){
+    var b=document.getElementById("scrollToFetch_bottom");
+    b=b.getBoundingClientRect();
+    const bottomY=b.top;
+    let viewportHeight=0;
+    if(!this.props.scrollParent){
+      viewportHeight=document.documentElement.clientHeight;
+    }else{
+      viewportHeight=document.getElementById(this.props.scrollParent).clientHeight;
+    }
+    
+    // console.log('top',b.top,'viewport',document.documentElement.clientHeight);
+    if(bottomY-viewportHeight <=viewportHeight/10){
+      //bottom inside viewport
+      if(!this.state.fetching){
+        this.setState({fetching:true});
+        await this.props.fetch(this.state.page);
+        this.setState({fetching:false,page:this.state.page+1})
+      }
+      
+    }
+  }
   }
 
   render() {
-    const {
-      text
-    } = this.props
-
+    let {finished,successMessage,loader,children}=this.props;
+    if(!loader){
+      loader=<div>loading...</div>
+    }
+    if(!successMessage){
+      successMessage=<div style={{textAlign:'center'}}>No more data to load</div>
+    }
     return (
-      <div className={styles.test}>
-        Example Component: {text}
+      <div id="scrollToFetch_container" style={{margin:20}}>
+        {children}
+        <div id="scrollToFetch_bottom"></div>
+        {finished ? successMessage:loader}
+        {}
       </div>
     )
   }
