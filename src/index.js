b275969234481c569758a8f3bb90b5815e87e337
Your Name <you@example.com>
2019-04-21 17:19:54 +0530

    an error regarding babel-core has been fixed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-require("babel-core/register");
+// require("babel-core/register");
 require("babel-polyfill");
 export default class ScrollToFetch extends Component {
   constructor(){
